eb1a0aa458b86230758dc5c9b279e6dae6a3732f
光太郎 <>
2026-02-01 13:06:26 +0900

    課題修正

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -11,22 +11,13 @@
    <p>
 <?php
 $personal_data = [
-    "name" => "玉ねぎ",
-    "price" => "200",
-    "lcocation" => "北海道"
+    "名前" => "玉ねぎ",
+    "値段" => "200",
+    "産地" => "北海道"
 ];
-
-echo $personal_data['name'];
-echo $personal_data['price'];
-echo $personal_data['lcocation'];
-
 
    foreach ($personal_data as $key => $value) {
             echo "{$key}:{$value}<br>";
-        }
-
-        foreach ($personal_data as $value) {
-            echo $value . '<br>';
         }
         
         ?>
